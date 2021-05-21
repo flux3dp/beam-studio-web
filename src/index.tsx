6db2671475c78755d88590d7dfb02237f2d9b867
Yanting Chen <mx.alexender@gmail.com>
2021-05-21 14:16:18 +0800

    build web

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import './main';
 import App from './App';
 
 if (process.env.NODE_ENV !== 'production') {
+  // eslint-disable-next-line no-console
   console.log('Looks like we are in development mode!');
 }
 
