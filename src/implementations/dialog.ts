9ac19947371273b3b4ca1dc6cfe24f49c4e8e165
Dean Sung <a71124725@gmail.com>
2021-06-10 16:36:40 +0800

    Update FileSaver.js to implement save dialog

--- src/implementations/dialog.ts
+++ src/implementations/dialog.ts
@@ -1,4 +1,70 @@
+import { saveAs } from 'file-saver';
+
 import { DialogFilter, IDialog, OpenDialogProperties } from 'core-interfaces/IDialog';
+
+// window.showOpenFilePicker and window.showSaveFilePicker are another options.
+// But they are not supported by Firefox currently.
+
+const mimetypeMap: { [key: string]: string } = {
+  json: 'application/json',
+  jpg: 'image/jpeg',
+  jpeg: 'image/jpeg',
+  png: 'image/png',
+  svg: 'image/svg+xml',
+  txt: 'text/plain',
+};
+
+const input = document.createElement('input');
+let lastPromiseResolve: ((result: { canceled?: boolean }) => void) | null;
+
+const openFileDialog = (options: {
+  defaultPath?: string,
+  filters?: DialogFilter[],
+  properties?: OpenDialogProperties[],
+}): Promise<{ filelist?: FileList | null, value?: string, canceled?: boolean }> => {
+  // TODO: defaultPath and contrain to file or directory
+  if (lastPromiseResolve) {
+    lastPromiseResolve({ canceled: true });
+  }
+  const { filters } = options;
+  input.setAttribute('type', 'file');
+  if (filters) {
+    let acceptAll = false;
+    const accept = [];
+    for (let i = 0; i < filters.length; i += 1) {
+      const filter = filters[i];
+      for (let j = 0; j < filter.extensions.length; j += 1) {
+        const extension = filter.extensions[j];
+        if (extension === '*') {
+          acceptAll = true;
+          break;
+        } else if (extension) {
+          accept.push(`.${extension}`);
+        }
+      }
+      if (acceptAll) {
+        break;
+      }
+    }
+    if (!acceptAll) {
+      input.setAttribute('accept', accept.join(','));
+    }
+  }
+  return new Promise((resolve) => {
+    lastPromiseResolve = resolve;
+    input.onchange = () => {
+      const { files, value } = input;
+      lastPromiseResolve = null;
+      resolve({
+        filelist: files,
+        value,
+      });
+    };
+
+    // input.addEventListener('')
+    input.click();
+  });
+};
 
 export default {
   async showSaveDialog(
@@ -8,6 +74,32 @@ export default {
   ): Promise<string | null> {
     return Promise.resolve(null);
   },
+  async writeFileDialog(
+    getContent: () => string | Blob | Promise<string | Blob>,
+    title?: string,
+    defaultPath?: string,
+    filters?: DialogFilter[],
+  ): Promise<string | null> {
+    const content = await getContent();
+    let dataType;
+    let fileName = defaultPath || '';
+    if (filters && filters.length > 0) {
+      const extension = filters[0].extensions[0];
+      if (extension && extension !== '*') {
+        if (extension in mimetypeMap) {
+          dataType = mimetypeMap[extension];
+        } else if (!fileName.includes('.')) {
+          fileName = `${fileName}.${extension}`;
+        }
+      }
+    }
+    const data = new Blob([content], { type: dataType });
+    if (fileName.startsWith('.')) {
+      fileName = ` ${fileName}`;
+    }
+    saveAs(data, fileName);
+    return null;
+  },
   async showOpenDialog(options: {
     defaultPath?: string,
     filters?: DialogFilter[],
@@ -16,9 +108,19 @@ export default {
       canceled: boolean,
       filePaths: string[],
     }> {
-    return Promise.resolve({
+    const { value } = await openFileDialog(options);
+    return {
       canceled: false,
-      filePaths: [],
-    });
+      filePaths: value ? [value] : [],
+    };
+  },
+  async getFileFromDialog(options: {
+    defaultPath?: string,
+    filters?: DialogFilter[],
+    properties?: OpenDialogProperties[],
+  }): Promise<Blob | File | null> {
+    const { filelist, canceled } = await openFileDialog(options);
+    if (canceled || !filelist) return null;
+    return filelist[0];
   },
 } as IDialog;
