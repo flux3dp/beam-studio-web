5fce72ca0355abf264872b65f83717fb2d276752
Yanting Chen <ChenYanting@users.noreply.github.com>
2021-08-16 18:13:08 +0800

    should display #801 popup error while doing actions which require device connection (#83)

---
+++ cypress/integration/top-bar/device-disconnection.spec.ts
@@ -0,0 +1,23 @@
+describe('verify top bar behaviors under device disconnection', () => {
+  beforeEach(() => {
+    cy.landing();
+  });
+
+  it('show #801 while clicking on preview button', () => {
+    cy.get('div.preview-button-container').should('exist');
+    cy.get('div.preview-button-container').click().click();
+    cy.get('pre.message').contains('#801');
+  });
+
+  it('show #801 while clicking on GO button', () => {
+    cy.get('div.go-btn').should('exist');
+    cy.get('div.go-btn').click();
+    cy.get('pre.message').contains('#801');
+  });
+
+  it('show #801 while clicking on path-preview button', () => {
+    cy.get('div.path-preview-button').should('exist');
+    cy.get('div.path-preview-button').click();
+    cy.get('pre.message').contains('#801');
+  });
+});
