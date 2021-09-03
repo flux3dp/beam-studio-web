23ca9ec18ae911357196af3bab954f61404727a0
Jess <jess@flux3dp.com>
2021-09-03 17:25:36 +0800

    add new testing about create object in canvas and text tool(70%)

--- cypress/integration/canvas/drawing.spec.ts
+++ cypress/integration/canvas/drawing.spec.ts
@@ -55,4 +55,46 @@ describe('drawing', () => {
     cy.get('div.onoffswitch').click();
     cy.get('#svg_1').should('have.attr', 'fill').and('eq', 'black');
   });
+
+  it('line', () => {
+    cy.get('div#left-Line>img').click();
+    cy.get('g#selectorParentGroup').should('have.css', 'cursor', 'crosshair');
+
+    cy.get('svg#svgcontent').trigger('mousedown', 100, 100, { force: true });
+    cy.get('svg#svgcontent').trigger('mousemove', 200, 200, { force: true });
+    cy.get('svg#svgcontent').trigger('mouseup', { force: true });
+
+    cy.get('#svg_1').should('exist');
+    cy.get('div.top-bar div.element-title').should('have.text', 'Layer 1 > Line');
+    cy.get('div#object-panel').should('exist')
+  });
+
+  it('pen', () => {
+    cy.get('div#left-Pen>img').click();
+    cy.get('g#selectorParentGroup').should('have.css', 'cursor', 'crosshair');
+
+    cy.get('svg#svgcontent').trigger('mousedown', 100, 100, { force: true });
+    cy.get('svg#svgcontent').trigger('mouseup', { force: true });
+    cy.get('svg#svgcontent').trigger('mousedown', 200, 200, { force: true });
+    cy.get('svg#svgcontent').trigger('mouseup', { force: true });
+    cy.get('svg#svgcontent').trigger('mousedown', 200, 0, { force: true });
+    cy.get('svg#svgcontent').trigger('mouseup', { force: true });
+    cy.get('svg#svgcontent').trigger('mousedown', 0, 200, { force: true });
+    cy.get('svg#svgcontent').trigger('mousedown', 0, 0, { force: true });
+
+    cy.get('#svg_1').should('exist');
+    cy.get('div#pathedit-panel').should('exist');
+  });
+
+  it('text', () => {
+    cy.get('div#left-Text>img').click();
+    cy.get('g#selectorParentGroup').should('have.css', 'cursor', 'move');
+
+    cy.get('svg#svgcontent').realClick({ x: 100, y: 200 }).realType('Bring Any Design to Life');
+    cy.get('div.top-bar div.element-title').should('have.text', 'Layer 1 > Text');
+
+    cy.get('#svg_1').should('exist');
+    cy.get('div.text-options').should('exist');
+    cy.get('#svg_1').should('have.text', 'Bring Any Design to Life');
+  });
 });
