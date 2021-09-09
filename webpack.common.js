b7e79b511f9bf8bd1b2e6d147c1ee8f863c5c939
Dean Sung <dean@flux3dp.com>
2021-09-09 10:48:48 +0800

    Add assets (#104)
    * Add touch tutorial assests
    
    * Update webpack config
    
    * Add camera calibration assets
    
    * Update touch-select.webm

--- webpack.common.js
+++ webpack.common.js
@@ -110,6 +110,7 @@ module.exports = {
     new CopyPlugin({
       patterns: [
         { from: path.resolve(__dirname, 'src/assets/images'), to: path.resolve(__dirname, 'dist/img') },
+        { from: path.resolve(__dirname, 'src/assets/video'), to: path.resolve(__dirname, 'dist/video') },
         { from: path.resolve(__dirname, 'public/js/lib/svgeditor/extensions'), to: path.resolve(__dirname, 'dist/js/lib/svgeditor/extensions') },
         { from: path.resolve(__dirname, 'public/js/lib/svgeditor/images'), to: path.resolve(__dirname, 'dist/js/lib/svgeditor/images') },
         { from: path.resolve(__dirname, 'public/js/lib/svg-nest'), to: path.resolve(__dirname, 'dist/js/lib/svg-nest') },
