6813ec003fc5cf2d7a9c99a109c7fa6362eab8cf
Jess <jess@flux3dp.com>
2021-09-10 10:18:40 +0800

    change doublequote to singlequote

--- cypress/integration/canvas/drawing.spec.ts
+++ cypress/integration/canvas/drawing.spec.ts
@@ -16,12 +16,12 @@ describe('drawing', () => {
     cy.get('div#object-panel').should('exist');
 
     cy.get('#selectorGrip_resize_ne').first().should(($grip) => {
-      expect(+$grip.attr("cx")).to.be.closeTo(301, 1);
-      expect(+$grip.attr("cy")).to.be.closeTo(99.5, 1);
+      expect($grip.attr('cx')).to.be.closeTo(301, 1);
+      expect($grip.attr('cy')).to.be.closeTo(99.5, 1);
     });
     cy.get('#selectorGrip_resize_sw').first().should(($grip) => {
-      expect(+$grip.attr("cx")).to.be.closeTo(100, 1);
-      expect(+$grip.attr("cy")).to.be.closeTo(200, 1);
+      expect($grip.attr('cx')).to.be.closeTo(100, 1);
+      expect($grip.attr('cy')).to.be.closeTo(200, 1);
     });
 
     cy.get('div.onoffswitch').click();
@@ -40,8 +40,8 @@ describe('drawing', () => {
     cy.get('div.top-bar div.element-title').should('have.text', 'Layer 1 > Oval');
     cy.get('div#object-panel').should('exist');
 
-    cy.get('#selectorGrip_resize_ne').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(400, 2); });
-    cy.get('#selectorGrip_resize_sw').first().should(($grip) => { expect(+$grip.attr("cy")).to.be.closeTo(400, 2); });
+    cy.get('#selectorGrip_resize_ne').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(400, 2); });
+    cy.get('#selectorGrip_resize_sw').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(400, 2); });
 
     cy.get('div.onoffswitch').click();
     cy.get('#svg_1').should('have.attr', 'fill').and('eq', '#000');
@@ -59,8 +59,8 @@ describe('drawing', () => {
     cy.get('div.top-bar div.element-title').should('have.text', 'Layer 1 > Polygon');
     cy.get('div#object-panel').should('exist');
 
-    cy.get('#selectorGrip_resize_ne').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(453, 2); });
-    cy.get('#selectorGrip_resize_sw').first().should(($grip) => { expect(+$grip.attr("cy")).to.be.closeTo(453, 2); });
+    cy.get('#selectorGrip_resize_ne').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(453, 2); });
+    cy.get('#selectorGrip_resize_sw').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(453, 2); });
     cy.wait(500);
 
     cy.get('div.option-input > input').clear().type('8').blur();
@@ -78,8 +78,8 @@ describe('drawing', () => {
     cy.get('svg#svgcontent').trigger('mousemove', 200, 200, { force: true });
     cy.get('svg#svgcontent').trigger('mouseup', { force: true });
 
-    cy.get('#selectorGrip_resize_ne').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(301, 1); });
-    cy.get('#selectorGrip_resize_nw').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(100, 1); });
+    cy.get('#selectorGrip_resize_ne').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(301, 1); });
+    cy.get('#selectorGrip_resize_nw').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(100, 1); });
 
     cy.get('#svg_1').should('exist');
     cy.get('div.top-bar div.element-title').should('have.text', 'Layer 1 > Line');
@@ -102,10 +102,10 @@ describe('drawing', () => {
     cy.get('#svg_1').should('exist');
     cy.get('div#pathedit-panel').should('exist');
 
-    cy.get('#pathpointgrip_0').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(100, 1); });
-    cy.get('#pathpointgrip_1').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(300, 1); });
-    cy.get('#pathpointgrip_2').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(301, 1); });
-    cy.get('#pathpointgrip_3').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(100, 1); });
+    cy.get('#pathpointgrip_0').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(100, 1); });
+    cy.get('#pathpointgrip_1').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(300, 1); });
+    cy.get('#pathpointgrip_2').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(301, 1); });
+    cy.get('#pathpointgrip_3').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(100, 1); });
   });
 
   it('text', () => {
--- cypress/integration/left-panel/pen-tools.spec.ts
+++ cypress/integration/left-panel/pen-tools.spec.ts
@@ -15,10 +15,10 @@ it('curve', () => {
   cy.get('#svg_1').should('exist');
   cy.get('#drawingCtrlPoint_0c2').should('exist');
 
-  cy.get('#pathpointgrip_0').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(100, 1); });
-  cy.get('#pathpointgrip_0').first().should(($grip) => { expect(+$grip.attr("cy")).to.be.closeTo(100, 1); });
-  cy.get('#pathpointgrip_1').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(250, 1); });
-  cy.get('#pathpointgrip_0').first().should(($grip) => { expect(+$grip.attr("cy")).to.be.closeTo(100, 1); });
+  cy.get('#pathpointgrip_0').first().should(($grip) => { expect(+$grip.attr('cx')).to.be.closeTo(100, 1); });
+  cy.get('#pathpointgrip_0').first().should(($grip) => { expect(+$grip.attr('cy')).to.be.closeTo(100, 1); });
+  cy.get('#pathpointgrip_1').first().should(($grip) => { expect(+$grip.attr('cx')).to.be.closeTo(250, 1); });
+  cy.get('#pathpointgrip_0').first().should(($grip) => { expect(+$grip.attr('cy')).to.be.closeTo(100, 1); });
 });
 
 it('tCorner', () => {
@@ -28,20 +28,20 @@ it('tCorner', () => {
     .trigger('mousedown', { which: 1, pageX: 50, pageY: 50 })
     .trigger('mousemove', { which: 1, pageX: 100, pageY: 400 })
     .trigger('mouseup')
-  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(650, 1); });
-  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr("cy")).to.be.closeTo(400, 1); });
+  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr('cx')).to.be.closeTo(650, 1); });
+  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr('cy')).to.be.closeTo(400, 1); });
 });
 
 it('tSmooth', () => {
   cy.get('[title="tSmooth"]').click();
 
-  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(567, 1); });
-  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr("cy")).to.be.closeTo(132, 1); });
+  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr('cx')).to.be.closeTo(567, 1); });
+  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr('cy')).to.be.closeTo(132, 1); });
 });
 
 it('tSymmetry', () => {
   cy.get('[title="tSymmetry"]').click();
 
-  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr("cx")).to.be.closeTo(600, 1); });
-  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr("cy")).to.be.closeTo(50, 1); });
+  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr('cx')).to.be.closeTo(600, 1); });
+  cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect(+$grip.attr('cy')).to.be.closeTo(50, 1); });
 });
