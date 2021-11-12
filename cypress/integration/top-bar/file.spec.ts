8d9b6e6545edcd890241d074122d0e2b14ccaabe
Jess <68686011+Jess-Jian@users.noreply.github.com>
2021-11-12 17:59:51 +0800

    feature/cypress/file (#128)
    * add the test about file
    
    * use the global variables

---
+++ cypress/integration/top-bar/file.spec.ts
@@ -0,0 +1,98 @@
+import { md5 } from '../../support/utils';
+
+describe('manipulate file', () => {
+  beforeEach(() => {
+    cy.landingEditor();
+  });
+
+  it('open file ', () => {
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(1) > .rc-menu > :nth-child(1)').click();
+    cy.get('input#file-input').then($input => {
+      cy.fixture('flux.png', 'base64')
+        .then(Cypress.Blob.base64StringToBlob)
+        .then(blob => {
+          const el = $input[0];
+          const testFile = new File([blob], 'flux.png', { type: 'image/png' });
+          const dataTransfer = new DataTransfer();
+          dataTransfer.items.add(testFile);
+          el.files = dataTransfer.files;
+          return cy.wrap($input).first().trigger('change', { force: true });
+        });
+    });
+    cy.get('#svg_1').should('exist');
+    cy.get('#width').should('have.value', '300');
+    cy.get('#height').should('have.value', '210');
+  });
+
+  it('save file', () => {
+    const cypressDownloadBeamPath = Cypress.env('cypressDownloadBeamPath');
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(1) > .rc-menu > :nth-child(3)').click();
+    cy.wait(5000);
+    cy.readFile(cypressDownloadBeamPath).then((info) => {
+      expect(md5(info)).equal('b6403a0144502172f9a92672c7a39d7b');
+    });
+  });
+
+  it('save as file', () => {
+    const cypressDownloadNewBeamPath = Cypress.env('cypressDownloadNewBeamPath');
+    cy.get('div#left-Rectangle>img').click();
+    cy.get('svg#svgcontent').trigger('mousedown', 100, 100, { force: true });
+    cy.get('svg#svgcontent').trigger('mousemove', 400, 400, { force: true });
+    cy.get('svg#svgcontent').trigger('mouseup', { force: true });
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(1) > .rc-menu > :nth-child(4)').click();
+    cy.wait(5000);
+    cy.readFile(cypressDownloadNewBeamPath).then((info) => {
+      expect(md5(info)).equal('014673ddffcc53595306b2bc18bd9ed3');
+    });
+  });
+
+  it('export bvg file ', () => {
+    const cypressDownloadBvgPath = Cypress.env('cypressDownloadBvgPath');
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu > :nth-child(1)').click();
+    cy.readFile(cypressDownloadBvgPath).then((info) => {
+      expect(md5(info)).equal('f3e871d07847f5ee0bfc7590274faf75');
+    });
+  });
+
+  it('export svg file ', () => {
+    const cypressDownloadSvgPath = Cypress.env('cypressDownloadSvgPath');
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu > :nth-child(2)').click();
+    cy.readFile(cypressDownloadSvgPath).then((info) => {
+      expect(md5(info)).equal('ab7e70d8e831d4a810d7182a9d108ec0');
+    });
+  });
+
+  it('export png file ', () => {
+    const cypressDownloadPngPath = Cypress.env('cypressDownloadPngPath');
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu > :nth-child(3)').click();
+    cy.readFile(cypressDownloadPngPath).then((info) => {
+      expect(md5(info)).equal('b20a5f0d14f9b36425dca6e22ff2712c');
+    });
+  });
+
+  it('export jpg file ', () => {
+    const cypressDownloadJpgPath = Cypress.env('cypressDownloadJpgPath');
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu > :nth-child(4)').click();
+    cy.readFile(cypressDownloadJpgPath).then((info) => {
+      expect(md5(info)).equal('4e8c0a4d941da85f10ad6226f45fd592');
+    });
+  });
+});
