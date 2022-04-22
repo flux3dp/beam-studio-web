684c5e5f2c98df36dad01e3a2fb295dee2a091b9
Dean Sung <a71124725@gmail.com>
2022-04-22 11:19:34 +0800

    Add disable onbeforeunload event

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -11,9 +11,6 @@
 //
 // -- This is a parent command --
 Cypress.Commands.add('landingEditor', (opts = {}) => {
-  const username = Cypress.env('username');
-  const password = Cypress.env('password');
-
   window.localStorage.setItem('printer-is-ready', 'true');
   window.localStorage.setItem('keep-flux-id-login', 'true');
   window.localStorage.setItem('enable-sentry', '0');
@@ -24,11 +21,11 @@ Cypress.Commands.add('landingEditor', (opts = {}) => {
   window.localStorage.setItem('last-installed-version', 'web');
   window.localStorage.setItem('questionnaire-version', '9999');
   cy.visit('/#/initialize/connect/flux-id-login', opts);
-  cy.get('input#email-input').type(username);
-  cy.get('input#password-input').type(password);
-  cy.get('div.primary').click();
-  cy.get('button[data-test-key="ok"]').click();
+  cy.get('.skip').click();
   // time for svgcanvas loading
+  cy.window().then((win) => {
+    win.onbeforeunload = null;
+  });
   cy.wait(500);
 });
 
