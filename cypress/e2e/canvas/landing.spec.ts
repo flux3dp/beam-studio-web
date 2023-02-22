f3b61b1a1d09e79ade37feae5fb17f7307c9787b
JohnnySung88 <johnnysung28@gmail.com>
2023-02-22 09:13:26 +0800

    Seperated all tests

--- cypress/e2e/canvas/landing.spec.ts
+++ cypress/e2e/canvas/landing.spec.ts
@@ -13,21 +13,16 @@ describe('landing', () => {
     cy.get('a.btn').click();
   });
 
-  it('flux login page/connection type selection page/land to canvas', () => {
-    const username = Cypress.env('username');
-    const password = Cypress.env('password');
-    cy.url().should('contain', '#/initialize/connect/flux-id-login');
-    cy.get('div.flux-login').should('exist');
-    cy.get('input#email-input').type(username);
-    cy.get('input#password-input').type(password);
-    cy.get('div.remember-me').click();
-    cy.get('button[class^="ant-btn"]').contains('Login').click();
+  it('connection type selection page', () => {
     cy.visit('#/initialize/connect/select-connection-type');
     cy.url({ timeout: 15000 }).should('contain', '#/initialize/connect/select-connection-type');
     cy.get('div.select-connection-type').should('exist');
     cy.get('div.btn-container').should('have.length', 4);
     cy.get('div.btn-page').click();
     window.localStorage.setItem('new-user', 'true');
+  });
+
+  it('land to canvas', () => {
     cy.visit('#/studio/beambox');
     cy.url({ timeout: 15000 }).should('contain', '#/studio/beambox');
 
@@ -44,6 +39,7 @@ describe('landing', () => {
           cy.get('button[class^="ant-btn"]').contains('ok').click();
         }
       });
+
     // Tutorial
     cy.get('body')
       .then((body) => {
@@ -58,4 +54,15 @@ describe('landing', () => {
       .find('div')
       .should('have.class', 'studio-container beambox-studio en');
   });
+
+  // it('flux login page', () => {
+  //   const username = Cypress.env('username');
+  //   const password = Cypress.env('password');
+  //   cy.url().should('contain', '#/initialize/connect/flux-id-login');
+  //   cy.get('div.flux-login').should('exist');
+  //   cy.get('input#email-input').type(username);
+  //   cy.get('input#password-input').type(password);
+  //   cy.get('div.remember-me').click();
+  //   cy.get('button[class^="ant-btn"]').contains('Login').click();
+  // });
 });
