3f4eb03760b8bde65004bb6b19cc555c281f4ddb
JohnnySung88 <johnnysung28@gmail.com>
2023-03-06 18:02:38 +0800

    Fix

--- cypress/e2e/canvas/landing.spec.ts
+++ cypress/e2e/canvas/landing.spec.ts
@@ -54,15 +54,4 @@ describe('landing', () => {
       .find('div')
       .should('have.class', 'studio-container beambox-studio en');
   });
-
-  // it('flux login page', () => {
-  //   const username = Cypress.env('username');
-  //   const password = Cypress.env('password');
-  //   cy.url().should('contain', '#/initialize/connect/flux-id-login');
-  //   cy.get('div.flux-login').should('exist');
-  //   cy.get('input#email-input').type(username);
-  //   cy.get('input#password-input').type(password);
-  //   cy.get('div.remember-me').click();
-  //   cy.get('button[class^="ant-btn"]').contains('Login').click();
-  // });
 });
--- cypress/e2e/top-bar/optimize.spec.ts
+++ cypress/e2e/top-bar/optimize.spec.ts
@@ -1,29 +1,29 @@
-// it('arrangement', () => {
-//   cy.landingEditor();
-//   cy.get('div.menu-btn-container').click();
-//   cy.get(':nth-child(2) > .rc-menu__item').click();
-//   cy.get(':nth-child(20) > .rc-menu__item').click();
-//   cy.get(':nth-child(20) > .rc-menu > .rc-menu__item').click();
-//   cy.get('.active > .text').should('exist').and('have.text', 'Arrange');
-//   cy.get(':nth-child(2) > .text').should('exist').and('have.text', 'Close');
+it('arrangement', () => {
+  cy.landingEditor();
+  cy.get('div.menu-btn-container').click();
+  cy.get(':nth-child(2) > .rc-menu__item').click();
+  cy.get(':nth-child(20) > .rc-menu__item').click();
+  cy.get(':nth-child(20) > .rc-menu > .rc-menu__item').click();
+  cy.get('.active > .text').should('exist').and('have.text', 'Arrange');
+  cy.get(':nth-child(2) > .text').should('exist').and('have.text', 'Close');
 
-//   cy.get('.active > .text').click();
-//   cy.get('.modal-alert').should('exist').and('have.text', 'Arrangement OptimizationThere is no element to arrange.OK');
-//   cy.get('[data-test-key="ok"]').click({ force: true });
+  cy.get('.active > .text').click();
+  cy.get('.ant-modal-content').should('exist').and('have.text', 'Arrangement OptimizationThere is no element to arrange.OK');
+  cy.contains('button span', 'OK').click()
 
-//   cy.get('div#left-Rectangle>img').click();
-//   cy.get('svg#svgcontent').trigger('mousedown', 100, 100, { force: true });
-//   cy.get('svg#svgcontent').trigger('mousemove', 200, 200, { force: true });
-//   cy.get('svg#svgcontent').trigger('mouseup', { force: true });
+  cy.get('div#left-Rectangle>img').click();
+  cy.get('svg#svgcontent').trigger('mousedown', 100, 100, { force: true });
+  cy.get('svg#svgcontent').trigger('mousemove', 200, 200, { force: true });
+  cy.get('svg#svgcontent').trigger('mouseup', { force: true });
 
-//   cy.get('#svg_1').click({ force: true });
-//   cy.get('.active > .text').click();
-//   cy.wait(10000);
-//   cy.get('.active > .text').should('have.text', 'Stop');
-//   cy.get('#svg_1').should(($location) => {
-//     let xLocation = $location.attr('x');
-//     let yLocation = $location.attr('y');
-//     expect(xLocation.toString().substring(0, 6)).equal('-0.000');
-//     expect(yLocation.toString().substring(0, 6)).equal('0.0000');
-//   });
-// });
+  cy.get('#svg_1').click({ force: true });
+  cy.get('.active > .text').click();
+  cy.wait(10000);
+  cy.get('.active > .text').should('have.text', 'Stop');
+  cy.get('#svg_1').should(($location) => {
+    let xLocation = $location.attr('x');
+    let yLocation = $location.attr('y');
+    expect(xLocation.toString().substring(0, 6)).equal('-0.000');
+    expect(yLocation.toString().substring(0, 6)).equal('0.0000');
+  });
+});
