5631ea3cceffe4f4a2f51342347c1637f3fe0bf2
JohnnySung88 <johnnysung28@gmail.com>
2023-03-06 12:18:59 +0800

    Fix

--- cypress/e2e/top-bar/file.spec.ts
+++ cypress/e2e/top-bar/file.spec.ts
@@ -26,54 +26,53 @@ describe('manipulate file', () => {
     cy.get('#height').should('have.value', '210');
   });
 
-  // FIXME: md5 would differ for computers.
-  // it('save file', () => {
-  //   const cypressDownloadBeamPath = Cypress.env('cypressDownloadBeamPath');
-  //   cy.get('div.menu-btn-container').click();
-  //   cy.get(':nth-child(1) > .rc-menu__item').click();
-  //   cy.get(':nth-child(1) > .rc-menu > :nth-child(3)').click();
-  //   cy.wait(5000);
-  //   cy.readFile(cypressDownloadBeamPath).then((info) => {
-  //     expect(md5(info)).equal('787f86ded62b8bda2abd030ed019cc20');
-  //   });
-  // });
+  it('save file', () => {
+    const cypressDownloadBeamPath = Cypress.env('cypressDownloadBeamPath');
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(1) > .rc-menu > :nth-child(3)').click();
+    cy.wait(1000);
+    cy.readFile(cypressDownloadBeamPath).then((info) => {
+      expect(md5(info)).equal('917032669987e291a375417a7701c15c');
+    });
+  });
 
-  // it('save as file', () => {
-  //   const cypressDownloadNewBeamPath = Cypress.env('cypressDownloadNewBeamPath');
-  //   cy.get('div#left-Rectangle>img').click();
-  //   cy.get('svg#svgcontent').trigger('mousedown', 100, 100, { force: true });
-  //   cy.get('svg#svgcontent').trigger('mousemove', 400, 400, { force: true });
-  //   cy.get('svg#svgcontent').trigger('mouseup', { force: true });
-  //   cy.get('div.menu-btn-container').click();
-  //   cy.get(':nth-child(1) > .rc-menu__item').click();
-  //   cy.get(':nth-child(1) > .rc-menu > :nth-child(4)').click();
-  //   cy.wait(5000);
-  //   cy.readFile(cypressDownloadNewBeamPath).then((info) => {
-  //     expect(md5(info)).equal('ce2b11bb03e5b8c69d83346584e21181');
-  //   });
-  // });
+  it('save as file', () => {
+    const cypressDownloadNewBeamPath = Cypress.env('cypressDownloadNewBeamPath');
+    cy.get('div#left-Rectangle>img').click();
+    cy.get('svg#svgcontent').trigger('mousedown', 100, 100, { force: true });
+    cy.get('svg#svgcontent').trigger('mousemove', 400, 400, { force: true });
+    cy.get('svg#svgcontent').trigger('mouseup', { force: true });
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(1) > .rc-menu > :nth-child(4)').click();
+    cy.wait(1000);
+    cy.readFile(cypressDownloadNewBeamPath).then((info) => {
+      expect(md5(info)).equal('39a9ac327155af2e54f8f4e2d47188e3');
+    });
+  });
 
-  // it('export bvg file ', () => {
-  //   const cypressDownloadBvgPath = Cypress.env('cypressDownloadBvgPath');
-  //   cy.get('div.menu-btn-container').click();
-  //   cy.get(':nth-child(1) > .rc-menu__item').click();
-  //   cy.get(':nth-child(8) > .rc-menu__item').click();
-  //   cy.get(':nth-child(8) > .rc-menu > :nth-child(1)').click();
-  //   cy.readFile(cypressDownloadBvgPath).then((info) => {
-  //     expect(md5(info)).equal('b93db596defc21b881c129b72f6588ec');
-  //   });
-  // });
+  it('export bvg file ', () => {
+    const cypressDownloadBvgPath = Cypress.env('cypressDownloadBvgPath');
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu > :nth-child(1)').click();
+    cy.readFile(cypressDownloadBvgPath).then((info) => {
+      expect(md5(info)).equal('594c05a5900bc9d162bb06042f0f3500');
+    });
+  });
 
-  // it('export svg file ', () => {
-  //   const cypressDownloadSvgPath = Cypress.env('cypressDownloadSvgPath');
-  //   cy.get('div.menu-btn-container').click();
-  //   cy.get(':nth-child(1) > .rc-menu__item').click();
-  //   cy.get(':nth-child(8) > .rc-menu__item').click();
-  //   cy.get(':nth-child(8) > .rc-menu > :nth-child(2)').click();
-  //   cy.readFile(cypressDownloadSvgPath).then((info) => {
-  //     expect(md5(info)).equal('327c9c92b0248d60e84c50965769398b');
-  //   });
-  // });
+  it('export svg file ', () => {
+    const cypressDownloadSvgPath = Cypress.env('cypressDownloadSvgPath');
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu > :nth-child(2)').click();
+    cy.readFile(cypressDownloadSvgPath).then((info) => {
+      expect(md5(info)).equal('eeec89beb519e13603b102de8be43ec9');
+    });
+  });
 
   it('export png file ', () => {
     const cypressDownloadPngPath = Cypress.env('cypressDownloadPngPath');
@@ -86,16 +85,16 @@ describe('manipulate file', () => {
     });
   });
 
-  // it('export jpg file ', () => {
-  //   const cypressDownloadJpgPath = Cypress.env('cypressDownloadJpgPath');
-  //   cy.get('div.menu-btn-container').click();
-  //   cy.get(':nth-child(1) > .rc-menu__item').click();
-  //   cy.get(':nth-child(8) > .rc-menu__item').click();
-  //   cy.get(':nth-child(8) > .rc-menu > :nth-child(4)').click();
-  //   cy.readFile(cypressDownloadJpgPath).then((info) => {
-  //     expect(md5(info)).equal('6ee6548bbfc243fd9ed37f973556f9a4');
-  //   });
-  // });
+  it('export jpg file ', () => {
+    const cypressDownloadJpgPath = Cypress.env('cypressDownloadJpgPath');
+    cy.get('div.menu-btn-container').click();
+    cy.get(':nth-child(1) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu__item').click();
+    cy.get(':nth-child(8) > .rc-menu > :nth-child(4)').click();
+    cy.readFile(cypressDownloadJpgPath).then((info) => {
+      expect(md5(info)).equal('6ee6548bbfc243fd9ed37f973556f9a4');
+    });
+  });
 
   it('upload file by drag and drop ', () => {
     cy.fixture('flux.png', 'base64')
