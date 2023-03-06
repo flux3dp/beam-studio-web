a573cd7b51b8a158999a91c54b59d9d50add0c15
JohnnySung88 <johnnysung28@gmail.com>
2023-03-06 15:45:25 +0800

    Fix undo-redo

--- cypress/e2e/top-bar/undo-redo.spec.ts
+++ cypress/e2e/top-bar/undo-redo.spec.ts
@@ -2,6 +2,7 @@ describe('verify undo/redo behaviors', () => {
   beforeEach(() => {
     cy.landingEditor();
   });
+
   it('image', () => {
     cy.uploadFile('flux.png', 'image/png');
     cy.get('#svg_1').should('exist');
@@ -30,18 +31,19 @@ describe('verify undo/redo behaviors', () => {
     checkBehaviors();
   });
 
-  // FIXME: Would fail with different default font
-  // it('text with font', () => {
-  //   text();
-  //   cy.get('div.react-select__value-container').click({ force: true });
-  //   cy.get('#react-select-2-option-36').click({ force: true });
-  //   cy.get('[src="img/top-bar/icon-undo.svg"]').click();
-  //   cy.get('#svg_1').click({ force: true });
-  //   cy.get('div.react-select__value-container').should('have.text', '思源黑體 TC');
-  //   cy.get('[src="img/top-bar/icon-redo.svg"]').click();
-  //   cy.get('#svg_1').click({ force: true });
-  //   cy.get('div.react-select__value-container').should('have.text', 'Noto Sans');
-  // });
+  it('text with font', () => {
+    text();
+    cy.get('div.react-select__value-container').click({ force: true });
+    cy.get('div[class*="react-select__menu-list"]').get('[alt*="思源黑体 SC"]').click({ force: true });
+    cy.get('#svg_1').click({ force: true });
+    cy.get('div.react-select__value-container').should('have.text', '思源黑体 SC');
+    cy.get('[src="img/top-bar/icon-undo.svg"]').click();
+    cy.get('#svg_1').click({ force: true });
+    cy.get('div.react-select__value-container').should('have.text', 'Noto Sans');
+    cy.get('[src="img/top-bar/icon-redo.svg"]').click();
+    cy.get('#svg_1').click({ force: true });
+    cy.get('div.react-select__value-container').should('have.text', '思源黑体 SC');
+  });
 
   it('text with style', () => {
     text();
@@ -54,27 +56,27 @@ describe('verify undo/redo behaviors', () => {
     cy.get('div.select-container > select').find('option:selected').should('have.text', 'Bold');
   });
 
-  // it('text with size', () => {
-  //   text();
-  //   cy.get(':nth-child(3) > div.option-input > input').clear({ force: true }).type('400').blur();
-  //   cy.get('[src="img/top-bar/icon-undo.svg"]').click();
-  //   cy.get('#svg_1').click({ force: true });
-  //   cy.get(':nth-child(3) > div.option-input > input').should('have.value', '200');
-  //   cy.get('[src="img/top-bar/icon-redo.svg"]').click();
-  //   cy.get('#svg_1').click({ force: true });
-  //   cy.get(':nth-child(3) > div.option-input > input').should('have.value', '400');
-  // });
+  it('text with size', () => {
+    text();
+    cy.get(':nth-child(3) > div.option-input > input').clear({ force: true }).type('400',{ force: true }).blur();
+    cy.get('[src="img/top-bar/icon-undo.svg"]').click();
+    cy.get('#svg_1').click({ force: true });
+    cy.get(':nth-child(3) > div.option-input > input').should('have.value', '200');
+    cy.get('[src="img/top-bar/icon-redo.svg"]').click();
+    cy.get('#svg_1').click({ force: true });
+    cy.get(':nth-child(3) > div.option-input > input').should('have.value', '400');
+  });
 
-  // it('text with letter spacing', () => {
-  //   text();
-  //   cy.get(':nth-child(4) > div.option-input > input').clear({ force: true }).type('1').blur();
-  //   cy.get('[src="img/top-bar/icon-undo.svg"]').click();
-  //   cy.get('#svg_1').click({ force: true });
-  //   cy.get(':nth-child(4) > div.option-input > input').should('have.value', '0');
-  //   cy.get('[src="img/top-bar/icon-redo.svg"]').click();
-  //   cy.get('#svg_1').click({ force: true });
-  //   cy.get(':nth-child(4) > div.option-input > input').should('have.value', '1');
-  // });
+  it('text with letter spacing', () => {
+    text();
+    cy.get(':nth-child(4) > div.option-input > input').clear({ force: true }).type('1',{ force: true }).blur();
+    cy.get('[src="img/top-bar/icon-undo.svg"]').click();
+    cy.get('#svg_1').click({ force: true });
+    cy.get(':nth-child(4) > div.option-input > input').should('have.value', '0');
+    cy.get('[src="img/top-bar/icon-redo.svg"]').click();
+    cy.get('#svg_1').click({ force: true });
+    cy.get(':nth-child(4) > div.option-input > input').should('have.value', '1');
+  });
 
   it('text with line spacing', () => {
     text();
@@ -87,19 +89,20 @@ describe('verify undo/redo behaviors', () => {
     cy.get(':nth-child(5) > div.option-input > input').should('have.value', '2');
   });
 
-  // FIXME: value varies with device
-  // it('text with vertical', () => {
-  //   text();
-  //   cy.get(':nth-child(6) > .onoffswitch > .onoffswitch-label > .onoffswitch-switch').click();
-  //   cy.get('[src="img/top-bar/icon-undo.svg"]').click();
-  //   cy.get('#svg_1').click({ force: true });
-  //   cy.get('#width').should('have.attr', 'value', '149.61');
-  //   cy.get('#height').should('have.attr', 'value', '28.97');
-  //   cy.get('[src="img/top-bar/icon-redo.svg"]').click();
-  //   cy.get('#svg_1').click({ force: true });
-  //   cy.get('#width').should('have.attr', 'value', '14.42');
-  //   cy.get('#height').should('have.attr', 'value', '288.97');
-  // });
+  it('text with vertical', () => {
+    text();
+    cy.get(':nth-child(6) > .onoffswitch > .onoffswitch-label > .onoffswitch-switch').click();
+    cy.get('[src="img/top-bar/icon-undo.svg"]').click();
+    cy.get('#svg_1').click({ force: true });
+    cy.wait(1000);
+    cy.get('#width').should('have.attr', 'value').then(parseFloat).should('be.greaterThan',100);
+    cy.get('#height').should('have.attr', 'value').then(parseFloat).should('be.lessThan',50);
+    cy.get('[src="img/top-bar/icon-redo.svg"]').click();
+    cy.get('#svg_1').click({ force: true });
+    cy.wait(1000);
+    cy.get('#width').should('have.attr', 'value').then(parseFloat).should('be.lessThan',50);
+    cy.get('#height').should('have.attr', 'value').then(parseFloat).should('be.greaterThan',100);
+  });
 
   it('text with infill', () => {
     text();
