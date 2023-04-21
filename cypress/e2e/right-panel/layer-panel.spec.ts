71e5610eef0ce23d7b2a24a74aa710a7befead46
Dean Sung <dean@flux3dp.com>
2023-04-21 15:52:19 +0800

    Fix layer e2e tests

--- cypress/e2e/right-panel/layer-panel.spec.ts
+++ cypress/e2e/right-panel/layer-panel.spec.ts
@@ -1,3 +1,5 @@
+const layerListClassPrefix = 'src-web-app-views-beambox-Right-Panels-LayerPanel-LayerList-module__';
+
 describe('manipulate layers', () => {
   beforeEach(() => {
     cy.landingEditor();
@@ -11,9 +13,9 @@ describe('manipulate layers', () => {
   });
 
   it('there is only one layer by default', () => {
-    cy.get('div.layer').should('have.length', 1);
-    cy.get('div.layer').should('have.class', 'current');
-    cy.get('div.layer div.layername').should('have.text', 'Layer 1');
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 1);
+    cy.get(`div[class*="${layerListClassPrefix}current"]`).should('have.length', 1);
+    cy.get(`div[class*="${layerListClassPrefix}item"] div[class*="${layerListClassPrefix}name"]`).should('have.text', 'Layer 1');
     cy.get('div#laser-panel div.layername').should('have.text', 'Parameter Settings (Layer 1)');
     cy.get('div#layerbackgroundColor-0').should('have.attr', 'style', 'background-color: rgb(51, 51, 51);')
   });
@@ -21,48 +23,48 @@ describe('manipulate layers', () => {
   it('add one new layer', () => {
     cy.get('div.add-layer-btn').should('exist');
     cy.get('div.add-layer-btn').click();
-    cy.get('div.layer').should('have.length', 2);
-    cy.get('div.layer:nth-child(1)').should('have.class', 'current');
-    cy.get('div.layer:nth-child(2)').should('not.have.class', 'current');
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 2);
+    cy.get(`div[class*="${layerListClassPrefix}current"]`).should('have.length', 1);
+    cy.get(`div[class*="${layerListClassPrefix}current"]`).should('have.attr', 'data-layer').should('eq', 'Layer 2');
     cy.get('div#laser-panel div.layername').should('have.text', 'Parameter Settings (Layer 2)');
     cy.get('div#layerbackgroundColor-1').should('have.attr', 'style', 'background-color: rgb(63, 81, 181);');
   });
 
   it('rename the new layer', () => {
-    cy.get('div.layer:nth-child(1)').dblclick();
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).dblclick();
     cy.get('input.text-input').clear().type('Hello Flux');
     cy.get('button[class^="ant-btn"]').contains('OK').click();
     cy.get('div#laser-panel div.layername').should('have.text', 'Parameter Settings (Hello Flux)');
   });
 
   it('delete the layer', () => {
-    cy.get('div.layer').trigger('mousedown', { button: 2 });
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).trigger('mousedown', { button: 2 });
     cy.get('#deletelayer').click({ force: true });
-    cy.get('div.layer').should('have.length', 1);
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 1);
     cy.get('div.add-layer-btn').click();
-    cy.get('div.layer').should('have.length', 2);
-    cy.get('div.layer:nth-child(2)').click({ force: true }).trigger('mousedown', { button: 2 });
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 2);
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(1).click({ force: true }).trigger('mousedown', { button: 2 });
     cy.get('#deletelayer').click({ force: true });
-    cy.get('div.layer').should('have.length', 1);
-    cy.get('div.layer div.layername').should('have.text', 'Layer 2');
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 1);
+    cy.get(`div[class*="${layerListClassPrefix}item"] div[class*="${layerListClassPrefix}name"]`).should('have.text', 'Layer 2');
   });
 
   it('duplicate the layer', () => {
-    cy.get('div.layer').trigger('mousedown', { button: 2 });
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).trigger('mousedown', { button: 2 });
     cy.get('#dupelayer').click({ force: true });
-    cy.get('div.layer').should('have.length', 2);
-    cy.get('div.layer:nth-child(1)').should('have.text', 'Layer 1 copy');
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 2);
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(0).should('have.text', 'Layer 1 copy');
     cy.get('#layerbackgroundColor-1').should('have.attr', 'style', 'background-color: rgb(51, 51, 51);');
   });
 
   it('drag the layer ', () => {
     cy.get('div.add-layer-btn').dblclick();
-    cy.get('div.layer[data-test-key="layer-2"]').dragTo('[data-index="0"]');
-    cy.get('#layerlist').children().first().should('have.attr', 'data-test-key', 'layer-2');
+    cy.get(`div[class*="${layerListClassPrefix}item"][data-testid="Layer 2"]`).dragTo('[data-index="0"]');
+    cy.get('#layerlist').children().last().should('have.attr', 'data-testid', 'Layer 2');
   });
 
   it('lock the layer ', () => {
-    cy.get('div.layer:nth-child(1)').trigger('mousedown', { button: 2 });
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(0).trigger('mousedown', { button: 2 });
     cy.get('#locklayer').click({ force: true });
     cy.get('#layerlock-0 > img').should('have.css', 'display', 'block');
     cy.get('#layerlock-0 > img').should('be.visible');
@@ -87,7 +89,7 @@ describe('manipulate layers', () => {
     cy.get('svg#svgcontent').trigger('mousemove', 300, 300, { force: true });
     cy.get('svg#svgcontent').trigger('mouseup', { force: true });
     cy.get('div.tab.layers').click();
-    cy.get('#layervis-0 > .vis-icon').click();
+    cy.get(`div[class*="${layerListClassPrefix}vis"]`).click();
     cy.get('#svg_1').should('be.hidden');
   });
 
@@ -95,19 +97,19 @@ describe('manipulate layers', () => {
     for (let n = 0; n < 9; n++) {
       cy.get('div.add-layer-btn').click();
     };
-    cy.get('div.layer').should('have.length', 10);
-    cy.get('div.layer:nth-child(1)').trigger('mousedown', { button: 2 });
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 10);
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(0).trigger('mousedown', { button: 2 });
     cy.get('#merge_all_layer').click({ force: true });
-    cy.get('div.layer').should('have.length', 1);
-    cy.get('div.layer:nth-child(1)').should('have.text', 'Layer 1');
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 1);
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(0).should('have.text', 'Layer 1');
   });
 
   it('merge down one layer', () => {
     cy.get('div.add-layer-btn').dblclick();
-    cy.get('div.layer').should('have.length', 3);
-    cy.get('div.layer:nth-child(1)').trigger('mousedown', { button: 2 });
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 3);
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(0).trigger('mousedown', { button: 2 });
     cy.get('#merge_down_layer').click({ force: true });
-    cy.get('div.layer').should('have.length', 2);
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 2);
     cy.get('#layerbackgroundColor-1').should('have.attr', 'style', 'background-color: rgb(63, 81, 181);');
   });
 
@@ -115,14 +117,14 @@ describe('manipulate layers', () => {
     for (let n = 0; n < 3; n++) {
       cy.get('div.add-layer-btn').click();
     };
-    cy.get('div.layer').should('have.length', 4);
-    cy.get('div.layer:nth-child(1)').should('have.text', 'Layer 4');
-    cy.get('div.layer:nth-child(1)').click();
-    cy.get('div.layer:nth-child(2)').trigger('mousedown', { button: 2, metaKey: true });
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 4);
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(0).should('have.text', 'Layer 4');
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(0).click();
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(1).trigger('mousedown', { button: 2, metaKey: true });
     cy.get('#merge_down_layer').click({ force: true });
-    cy.get('div.layer').should('have.length', 3);
-    cy.get('div.layer:nth-child(1)').should('have.text', 'Layer 3');
-    cy.get('div.layer:nth-child(2)').should('have.text', 'Layer 2');
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).should('have.length', 3);
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(0).should('have.text', 'Layer 3');
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(1).should('have.text', 'Layer 2');
     cy.get('#layerbackgroundColor-2').should('have.attr', 'style', 'background-color: rgb(244, 67, 54);');
   });
 
@@ -130,11 +132,11 @@ describe('manipulate layers', () => {
     cy.get('#laser-config-dropdown').select('Wood - 3mm Cutting');
     cy.get('div.add-layer-btn').click();
     cy.get('#laser-config-dropdown').select('Acrylic - 3mm Cutting');
-    cy.get('div.layer:nth-child(2)').click();
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(1).click();
     cy.get('#power').should('have.value', '60');
     cy.get('#speed').should('have.value', '6');
     cy.get('#repeat').should('have.value', '1');
-    cy.get('div.layer:nth-child(1)').click();
+    cy.get(`div[class*="${layerListClassPrefix}item"]`).eq(0).click();
     cy.get('#power').should('have.value', '60');
     cy.get('#speed').should('have.value', '8');
     cy.get('#repeat').should('have.value', '1');
