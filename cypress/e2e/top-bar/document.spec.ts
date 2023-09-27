8296adc31f085746b0d102daf345d0274ce075a2
Simon Ko <simon@flux3dp.com>
2023-09-27 19:20:18 +0800

    Fix document click issue

--- cypress/e2e/top-bar/document.spec.ts
+++ cypress/e2e/top-bar/document.spec.ts
@@ -24,7 +24,7 @@ describe('manipulate document setting', () => {
   it('working area of beambox', () => {
     openDocument();
     cy.get('[class^="ant-select-selection-item"]').eq(0).click();
-    cy.get('[class^="ant-select-item-option-content"]').contains('Beambox').click();
+    cy.get('[class^="ant-select-item-option-content"]').contains('Beambox').click({force: true});
     cy.get('button[class^="ant-btn"]').contains('Save').click();
     cy.get('#svgroot').should('have.attr', 'x', '4000');
     cy.get('#svgroot').should('have.attr', 'y', '3750');
@@ -33,7 +33,7 @@ describe('manipulate document setting', () => {
   it('working area of beambox pro', () => {
     openDocument();
     cy.get('[class^="ant-select-selection-item"]').eq(0).click();
-    cy.get('[class^="ant-select-item-option-content"]').contains('Beambox Pro').click();
+    cy.get('[class^="ant-select-item-option-content"]').contains('Beambox Pro').click({force: true});
     cy.get('button[class^="ant-btn"]').contains('Save').click();
     cy.get('#svgroot').should('have.attr', 'x', '6000');
     cy.get('#svgroot').should('have.attr', 'y', '3750')
@@ -42,9 +42,9 @@ describe('manipulate document setting', () => {
   it('working area of HEXA', () => {
     openDocument();
     cy.get('[class^="ant-select-selection-item"]').eq(0).click();
-    cy.get('[class^="ant-select-item-option-content"]').contains('HEXA').click();
+    cy.get('[class^="ant-select-item-option-content"]').contains('HEXA').click({force: true});
     cy.get('button[class^="ant-btn"]').contains('Save').click();
-    cy.get('#svgroot').should('have.attr', 'x', '7300');
+    cy.get('#svgroot').should('have.attr', 'x', '7400');
     cy.get('#svgroot').should('have.attr', 'y', '4100')
   });
 
