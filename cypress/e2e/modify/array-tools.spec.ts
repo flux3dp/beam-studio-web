4f2a00221241171baa7558676eda1d7b604ace9a
Dean Sung <dean@flux3dp.com>
2023-12-29 11:58:19 +0800

    Fix array-tool and mirror-tool e2e

--- cypress/e2e/modify/array-tools.spec.ts
+++ cypress/e2e/modify/array-tools.spec.ts
@@ -3,12 +3,26 @@ describe('array tools', () => {
     cy.landingEditor();
   });
 
+  const doAllThing = () => {
+    cy.get('div#left-Cursor>img').click();
+    cy.get('svg#svgcontent').trigger('mousedown', -10, -10, { force: true });
+    cy.get('svg#svgcontent').trigger('mousemove', 400, 400, { force: true });
+    cy.get('svg#svgcontent').trigger('mouseup', { force: true });
+    cy.wait(500);
+    cy.get('#array').click();
+    cy.get('#columns').clear().type('2').blur();
+    cy.get('#rows').clear().type('2').blur();
+    cy.get('#array_width').clear().type('100').blur();
+    cy.get('#array_height').clear().type('150').blur();
+    cy.get('.primary').click();
+  };
+
   it('image', () => {
     cy.uploadFile('flux.png', 'image/png');
     doAllThing();
     cy.wait(500);
     cy.get('div.top-bar div.element-title').should('have.text', 'Multiple Objects');
-    cy.get('#svg_5').children('image').should('have.length', '4');
+    cy.get('g[data-tempgroup="true"]').children('image').should('have.length', '4');
   });
 
   it('geometry', () => {
@@ -19,7 +33,7 @@ describe('array tools', () => {
     cy.wait(500);
     doAllThing();
     cy.get('div.top-bar div.element-title').should('have.text', 'Multiple Objects');
-    cy.get('polygon').should('have.length', '4');
+    cy.get('g[data-tempgroup="true"]').should('have.length', '4');
   });
 
   it('path', () => {
@@ -30,16 +44,17 @@ describe('array tools', () => {
     cy.wait(500);
     doAllThing();
     cy.get('div.top-bar div.element-title').should('have.text', 'Multiple Objects');
-    cy.get('#svg_5').children('line').should('have.length', '4');
+    cy.get('g[data-tempgroup="true"]').children('line').should('have.length', '4');
   });
 
   it('text', () => {
     cy.get('div#left-Text>img').click();
-    cy.get('svg#svgcontent').realClick({ x: 10, y: 20 }).realType('Test Array');
-    cy.wait(500);
+    cy.get('svg#svgcontent').realClick({ x: 10, y: 20 });
+    cy.get('#selectorGroup_svg_1', { timeout: 1000 }).should('exist');
+    cy.realType('Test Array');
     doAllThing();
     cy.get('div.top-bar div.element-title').should('have.text', 'Multiple Objects');
-    cy.get('#svg_8').children('text').should('have.length', '4');
+    cy.get('g[data-tempgroup="true"]').children('text').should('have.length', '4');
   });
 
   it('group', () => {
@@ -51,7 +66,7 @@ describe('array tools', () => {
     cy.get('svg#svgcontent').trigger('mouseup', { force: true });
     cy.wait(500);
     // Select both
-    cy.get('svg#svgcontent').trigger('mousedown', 50,50, { force: true });
+    cy.get('svg#svgcontent').trigger('mousedown', 50, 50, { force: true });
     cy.get('svg#svgcontent').trigger('mousemove', 600, 600, { force: true });
     cy.get('svg#svgcontent').trigger('mouseup', { force: true });
     cy.wait(500);
@@ -75,21 +90,11 @@ describe('array tools', () => {
     cy.wait(500);
     doAllThing();
     cy.get('div.top-bar div.element-title').should('have.text', 'Multiple Objects');
-    cy.get('#svg_13', { timeout: 3000 }).children('line').should('have.length', '4');
-    cy.get('polygon').should('have.length', '4');
+    cy.get('g[data-tempgroup="true"]', { timeout: 3000 })
+      .children('line')
+      .should('have.length', '4');
+    cy.get('g[data-tempgroup="true"]', { timeout: 3000 })
+      .children('polygon')
+      .should('have.length', '4');
   });
-
-  function doAllThing() {
-    cy.get('div#left-Cursor>img').click();
-    cy.get('svg#svgcontent').trigger('mousedown', -10, -10, { force: true });
-    cy.get('svg#svgcontent').trigger('mousemove', 400, 400, { force: true });
-    cy.get('svg#svgcontent').trigger('mouseup', { force: true });
-    cy.wait(500);
-    cy.get('#array').click();
-    cy.get('#columns').clear().type('2').blur();
-    cy.get('#rows').clear().type('2').blur();
-    cy.get('#array_width').clear().type('100').blur();
-    cy.get('#array_height').clear().type('150').blur();
-    cy.get('.primary').click();
-  };
 });
