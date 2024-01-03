1ab10d1a90c1385dfe723bfe0f35c448e6e0e293
Dean Sung <dean@flux3dp.com>
2024-01-03 19:15:58 +0800

    Fix pen tool due to cypress update

--- cypress/e2e/left-panel/pen-tools.spec.ts
+++ cypress/e2e/left-panel/pen-tools.spec.ts
@@ -1,9 +1,7 @@
 describe('pen tools', () => {
-  it('path curve', () => {
+  beforeEach(() => {
     cy.landingEditor();
-    cy.wait(500);
     cy.get('div#left-Pen>img').click();
-    cy.wait(500);
     cy.get('svg#svgcontent').trigger('mousedown', 100, 100, { force: true });
     cy.get('svg#svgcontent').trigger('mouseup', { force: true });
     cy.get('svg#svgcontent').trigger('mousedown', 150, 150, { force: true });
@@ -14,38 +12,57 @@ describe('pen tools', () => {
     cy.get('svg#svgcontent').trigger('mousedown', 400, 200, { force: true });
     cy.get('svg#svgcontent').trigger('mousemove', 500, -50, { force: true });
     cy.get('svg#svgcontent').dblclick({ force: true });
-    cy.wait(500);
     cy.get('#svg_1', { timeout: 7000 }).should('exist');
     cy.get('#drawingCtrlPoint_0c2').should('exist');
+  });
 
+  it('path curve', () => {
     cy.get('#pathpointgrip_0').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(100, 1); });
     cy.get('#pathpointgrip_0').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(100, 1); });
     cy.get('#pathpointgrip_1').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(250, 1); });
     cy.get('#pathpointgrip_0').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(100, 1); });
   });
 
-  it('path corner', () => {
+  it('path nodetype corner', () => {
+    cy.get('#pathpointgrip_3', { timeout: 7000 }).dblclick();
+    cy.get('#pathedit-panel').should('exist');
+    cy.get('[title="Corner"]').click();
+    cy.get('#ctrlpointgrip_4c1')
+      .trigger('mousedown', { which: 1, pageX: 50, pageY: 50 })
+      .trigger('mousemove', { which: 1, pageX: 100, pageY: 400 })
+      .trigger('mouseup');
+
+    cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(650, 1); });
+    cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(400, 1); });
+    cy.get('#ctrlpointgrip_3c2').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(400, 1); });
+    cy.get('#ctrlpointgrip_3c2').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(550, 1); });
+  });
+
+  it('path nodetype smooth', () => {
+    cy.get('#pathpointgrip_3', { timeout: 7000 }).dblclick();
+    cy.get('#pathedit-panel').should('exist');
+    cy.get('[title="Smooth"]').click();
+    cy.get('#ctrlpointgrip_4c1')
+      .trigger('mousedown', { which: 1, pageX: 50, pageY: 50 })
+      .trigger('mousemove', { which: 1, pageX: 100, pageY: 400 })
+      .trigger('mouseup');
+
+    cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(650, 1); });
+    cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(400, 1); });
+    cy.get('#ctrlpointgrip_3c2').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(277, 1); });
+    cy.get('#ctrlpointgrip_3c2').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(151, 1); });
+  });
+
+  it('path nodetype symmetry', () => {
     cy.get('#pathpointgrip_3', { timeout: 7000 }).dblclick();
     cy.get('#pathedit-panel').should('exist');
     cy.get('#ctrlpointgrip_4c1')
       .trigger('mousedown', { which: 1, pageX: 50, pageY: 50 })
       .trigger('mousemove', { which: 1, pageX: 100, pageY: 400 })
-      .trigger('mouseup')
+      .trigger('mouseup');
     cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(650, 1); });
     cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(400, 1); });
+    cy.get('#ctrlpointgrip_3c2').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(350, 1); });
+    cy.get('#ctrlpointgrip_3c2').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(200, 1); });
   });
-
-  // it('tSmooth', () => {
-  //   cy.get('[title="tSmooth"]').click();
-
-  //   cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(567, 1); });
-  //   cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(132, 1); });
-  // });
-
-  // it('tSymmetry', () => {
-  //   cy.get('[title="tSymmetry"]').click();
-
-  //   cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cx')).to.be.closeTo(600, 1); });
-  //   cy.get('#ctrlpointgrip_4c1').first().should(($grip) => { expect($grip.attr('cy')).to.be.closeTo(50, 1); });
-  // });
 });
