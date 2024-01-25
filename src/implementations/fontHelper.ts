cc05900a4eb8596d559748efa096dfeb92a91280
EstherFu <sloborfu@gmail.com>
2024-01-25 17:46:46 +0800

    Add fontkit

--- src/implementations/fontHelper.ts
+++ src/implementations/fontHelper.ts
@@ -1,5 +1,3 @@
-import progressCaller from 'app/actions/progress-caller';
-import getUtilWS from 'helpers/api/utils-ws';
 import i18n from 'helpers/i18n';
 import {
   FontDescriptor,
@@ -86,8 +84,6 @@ const findFonts = async (
 
 const applyMonotypeStyle = monotypeFonts.applyStyle;
 
-const fontDirectory = '/usr/share/fonts/truetype/beam-studio/';
-
 export default {
   findFont,
   findFonts,
@@ -104,95 +100,9 @@ export default {
     }
     return font.family as string;
   },
-  async getWebFontAndUpload(postscriptName: string) {
-    const utilWS = getUtilWS();
-    const font = (await getAvailableFonts()).find(
-      (f) => f.postscriptName === postscriptName
-    );
-    const fileName = font?.fileName || `${postscriptName}.ttf`;
-    const isMonotype = font && 'hasLoaded' in font;
-    const fontPath = isMonotype
-      ? `${fontDirectory}monotype/${fileName}`
-      : `${fontDirectory}${fileName}`;
-    const isExisting = await utilWS.checkExist(fontPath);
-    if (!isExisting) {
-      let isCanceled = false;
-      let message = i18n.lang.beambox.right_panel.object_panel.actions_panel.fetching_web_font;
-      await progressCaller.openSteppingProgress({
-        id: 'fetch-web-font',
-        message,
-        onCancel: () => {
-          isCanceled = true;
-        },
-      });
-      const { protocol } = window.location;
-      let url = `${protocol}//beam-studio-web.s3.ap-northeast-1.amazonaws.com/fonts/${fileName}`;
-      // TODO: move to cloud service or delete monotype file
-      if (isMonotype) {
-        const monotypeUrl = await monotypeFonts.getUrlWithToken(postscriptName);
-        if (monotypeUrl) url = monotypeUrl;
-      }
-      let resp = (await fetch(url, {
-        mode: 'cors',
-      })) as Response;
-      const contentType = resp.headers.get('content-type') as string;
-      if (contentType === 'application/json') {
-        console.error(await resp.json());
-        progressCaller.popById('fetch-web-font');
-        return false;
-      }
-      const contentLength = resp.headers.get('content-length') as string;
-      const total = parseInt(contentLength, 10);
-      let loaded = 0;
-
-      // getting progress of fetch
-      resp = new Response(new ReadableStream({
-        async start(controller) {
-          const reader = resp.body?.getReader();
-          if (!reader) {
-            controller.close();
-            return;
-          }
-          let done = false;
-          while (!done) {
-            // eslint-disable-next-line no-await-in-loop
-            const result = await reader.read();
-            done = result.done;
-            if (done) break;
-            const { value } = result;
-            if (value) {
-              loaded += value.byteLength;
-              progressCaller.update('fetch-web-font', { percentage: (loaded / total) * 100 });
-            }
-            controller.enqueue(value);
-          }
-          controller.close();
-        },
-      }));
-      if (resp.status !== 200) {
-        progressCaller.popById('fetch-web-font');
-        return false;
-      }
-      const blob = await resp.blob();
-      if (isCanceled) {
-        progressCaller.popById('fetch-web-font');
-        return false;
-      }
-      message = i18n.lang.beambox.right_panel.object_panel.actions_panel.uploading_font_to_machine;
-      progressCaller.update('fetch-web-font', { message, percentage: 0 });
-      try {
-        const res = await utilWS.uploadTo(blob, fontPath, (progress: number) => {
-          progressCaller.update('fetch-web-font', { percentage: 100 * progress });
-        });
-        progressCaller.popById('fetch-web-font');
-        if (!res || isCanceled) return false;
-      } catch (e) {
-        progressCaller.popById('fetch-web-font');
-        return false;
-      }
-    }
-    return true;
-  },
-  getWebFontPreviewUrl: (fontFamily: string) => (previewSourceMap[fontFamily] || null),
+  getWebFontPreviewUrl: (fontFamily: string) =>
+    previewSourceMap[fontFamily] || null,
   applyMonotypeStyle,
+  getMonotypeUrl: monotypeFonts.getUrlWithToken,
+  getLocalFont: () => undefined,
 } as FontHelper;
