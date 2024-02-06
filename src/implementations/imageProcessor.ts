f2eb349b8a615d2441b84c7f6b0672f2b24d921a
Dean Sung <dean@flux3dp.com>
2024-02-06 15:19:10 +0800

    Fix imageProcessor in worker

--- src/implementations/imageProcessor.ts
+++ src/implementations/imageProcessor.ts
@@ -1,12 +1,20 @@
+/// <reference lib="webworker" />
 import { IImageProcessor } from 'interfaces/IImage';
 
-const { Jimp } = window;
+// run this in global scope of window or worker. since window.self = window, we're ok
+if (typeof WorkerGlobalScope !== 'undefined' && global instanceof WorkerGlobalScope) {
+  importScripts('https://unpkg.com/jimp@0.16.1/browser/lib/jimp.js');
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+const { Jimp } = global;
 
 export default {
   MIME_PNG: Jimp.MIME_PNG,
   BLEND_OVERLAY: Jimp.BLEND_OVERLAY,
   AUTO: Jimp.AUTO,
-  read(data: Buffer): Promise<Jimp> {
+  read(data: Buffer) {
     return Jimp.read(data);
   },
 } as IImageProcessor;
