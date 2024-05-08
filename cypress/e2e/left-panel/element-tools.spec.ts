870431e11f06e21b74460c7114477a7da7acc697
Dean Sung <dean@flux3dp.com>
2024-05-08 17:54:59 +0800

    Fix element-tools

--- cypress/e2e/left-panel/element-tools.spec.ts
+++ cypress/e2e/left-panel/element-tools.spec.ts
@@ -1,6 +1,6 @@
 import { md5 } from '../../support/utils';
 
-const shapePanelPrefix = 'src-web-app-views-beambox-ShapePanel-ShapePanel-module__';
+const shapeIconPrefix = 'src-web-app-views-beambox-ShapePanel-ShapeIcon-module__';
 
 describe('import from element panel', () => {
   beforeEach(() => {
@@ -12,41 +12,24 @@ describe('import from element panel', () => {
     cy.get('.ant-modal-header').contains('Element').should('exist');
     cy.get('.ant-modal-body').should('exist');
     cy.get('.adm-capsule-tabs-tab.adm-capsule-tabs-tab-active').should('exist');
-    cy.get('.adm-capsule-tabs-tab.adm-capsule-tabs-tab-active').should('have.text', 'Shape');
-    cy.get(`.anticon[class*="${shapePanelPrefix}icon--"]`).eq(0).click();
+    cy.get('.adm-capsule-tabs-tab.adm-capsule-tabs-tab-active').should('have.text', 'Basic');
+    cy.get(`.anticon[class*="${shapeIconPrefix}icon--"]`).eq(0).click();
     cy.get('#svg_1').should('exist');
     cy.get('#svg_1').should('have.attr', 'cx', '250');
     cy.get('#svg_1').should('have.attr', 'cy', '250');
     cy.get('#svg_1').should('have.attr', 'fill', '#333333');
   });
 
-  it('import svg element', () => {
+  it('switch tab and import svg element', () => {
     cy.clickToolBtn('Element');
-    cy.get('.adm-capsule-tabs-tab-wrapper').contains('Graphics').click();
+    cy.get('.adm-capsule-tabs-tab-wrapper').contains('Decor').click();
     cy.get('.adm-capsule-tabs-tab.adm-capsule-tabs-tab-active').should('exist');
-    cy.get('.adm-capsule-tabs-tab.adm-capsule-tabs-tab-active').should('have.text', 'Graphics');
-    cy.get(`.anticon[class*="${shapePanelPrefix}icon--"]`).eq(0).click();
+    cy.get('.adm-capsule-tabs-tab.adm-capsule-tabs-tab-active').should('have.text', 'Decor');
+    cy.get(`.anticon[class*="${shapeIconPrefix}icon--"]`).eq(0).click();
     cy.get('#svg_9').should('exist');
     cy.get('#svg_9').should('have.attr', 'fill', '#333333');
     cy.get('#svg_9')
       .invoke('attr', 'd')
-      .then((html) => expect(md5(html)).equal('c6bc81cd8591fc0a1d2a4d77203d09e1'));
-  });
-
-  it('import line element', () => {
-    cy.clickToolBtn('Element');
-    cy.get('.adm-capsule-tabs-tab-wrapper').contains('Line').click();
-    cy.get('.adm-capsule-tabs-tab.adm-capsule-tabs-tab-active').should('exist');
-    cy.get('.adm-capsule-tabs-tab.adm-capsule-tabs-tab-active').should('have.text', 'Line');
-    cy.get(`.anticon[class*="${shapePanelPrefix}icon--"]`).eq(4).click();
-    cy.get('#svg_1').should('exist');
-    cy.get('#svg_1').should('have.attr', 'stroke', '#333333');
-    cy.get('#svg_1').should('have.attr', 'fill', 'none');
-    cy.get('#svg_1').should('have.attr', 'data-ratiofixed', 'true');
-    cy.get('#svg_1').should('have.attr', 'style', 'pointer-events:none');
-    cy.get('#svg_1').should('have.attr', 'vector-effect', 'non-scaling-stroke');
-    cy.get('#svg_1')
-      .invoke('attr', 'd')
-      .then((html) => expect(md5(html)).equal('0a946d3b75ebf64f8c1820b9f06c45f0'));
+      .then((html) => expect(md5(html)).equal('7e57d924a29e7ad9272398d9e6595eec'));
   });
 });
