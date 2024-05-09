9d11cf37d851bb7d16b01680d925483cc8e145b2
Dean Sung <dean@flux3dp.com>
2024-05-09 17:09:34 +0800

    Fix optimize.spec.ts

--- cypress/e2e/top-bar/optimize.spec.ts
+++ cypress/e2e/top-bar/optimize.spec.ts
@@ -1,28 +1,21 @@
 it('arrangement', () => {
   cy.landingEditor();
-  cy.get('div.menu-btn-container').click();
-  cy.get(':nth-child(2) > .rc-menu__item').click();
-  cy.get(':nth-child(20) > .rc-menu__item').click();
-  cy.get(':nth-child(20) > .rc-menu > .rc-menu__item').click();
-  cy.get('.active > .text').should('exist').and('have.text', 'Arrange');
-  cy.get(':nth-child(2) > .text').should('exist').and('have.text', 'Close');
-
-  cy.get('.active > .text').click();
-  cy.get('.ant-modal-content').should('exist').and('have.text', 'Arrangement OptimizationThere is no element to arrange.OK');
-  cy.contains('button span', 'OK').click()
-
+  cy.wait(100);
   cy.clickToolBtn('Rectangle');
   cy.get('svg#svgcontent').trigger('mousedown', 100, 100, { force: true });
   cy.get('svg#svgcontent').trigger('mousemove', 200, 200, { force: true });
   cy.get('svg#svgcontent').trigger('mouseup', { force: true });
 
   cy.get('#svg_1').click({ force: true });
-  cy.get('.active > .text').click();
+  cy.get('.tab.objects').click();
+  cy.contains('Auto Arrange').click();
+  cy.wait(100);
+  cy.get('.svg-nest-button.active').click();
   cy.wait(10000);
-  cy.get('.active > .text').should('have.text', 'Stop');
+  cy.get('.svg-nest-button.active > .text').should('have.text', 'Stop');
   cy.get('#svg_1').should(($location) => {
-    let xLocation = $location.attr('x');
-    let yLocation = $location.attr('y');
+    const xLocation = $location.attr('x');
+    const yLocation = $location.attr('y');
     expect(xLocation.toString().substring(0, 6)).equal('-0.000');
     expect(yLocation.toString().substring(0, 6)).equal('0.0000');
   });
