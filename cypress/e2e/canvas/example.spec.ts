6234aa3ff4d9fcfaf5721d0b18c3e60bc76f7d4c
Dean Sung <dean@flux3dp.com>
2024-05-14 17:10:58 +0800

    Fix example e2e

--- cypress/e2e/canvas/example.spec.ts
+++ cypress/e2e/canvas/example.spec.ts
@@ -7,6 +7,7 @@ function checkExample(exampleName: string, res: string | undefined) {
   cy.get('.rc-menu__item').contains(exampleName).click();
 
   if (res) {
+    cy.findByTestId('Layer 1', { timeout: 10000 }).should('not.exist');
     cy.get('#svgcontent')
       .invoke('prop', 'outerHTML')
       .then((html) => expect(md5(html)).equal(res));
@@ -20,45 +21,45 @@ function checkExample(exampleName: string, res: string | undefined) {
 const exampleList = [
   {
     label: 'Example of Ador Laser',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
+    adorRes: '21094e29e4aa055e51f2579e0ddcdcc9',
   },
   {
     label: 'Example of Ador Printing - Single Color',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
+    adorRes: '1f6a7c0a62535d5f7adbbd96f8084f19',
   },
   {
     label: 'Example of Ador Printing - Full Color',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
+    adorRes: 'f5fae608f9a4e41d205bdb77edad7d55',
   },
   {
     label: 'Example of beamo',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
-    beamseriesRes: '92aa987b1188bf082911e23cbaabb51b',
+    adorRes: 'bc3f0fed38f408ae26316944269c76ea',
+    beamseriesRes: '17d386fdbc8e27f34447fa4c175b8fb8',
   },
   {
     label: 'Example of beambox',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
-    beamseriesRes: '92aa987b1188bf082911e23cbaabb51b',
+    adorRes: 'ea5a3e9f7dfbf480369d19fb79a85d94',
+    beamseriesRes: '803189fb875a8ee7bdcec110acf407ec',
   },
   {
     label: 'Material Engraving Test',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
-    beamseriesRes: '92aa987b1188bf082911e23cbaabb51b',
+    adorRes: '22f173149936c6013852f36b4c8c7d2c',
+    beamseriesRes: '000023114baaf69be16cc7d3a5a1ffa8',
   },
   {
     label: 'Material Engraving Test - Classic',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
-    beamseriesRes: '92aa987b1188bf082911e23cbaabb51b',
+    adorRes: 'c8106ce4d8aa62f980bbfc2f67ed218e',
+    beamseriesRes: '3e9e5c89bef49a6db995bff71cab9c98',
   },
   {
     label: 'Material Cutting Test',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
+    adorRes: 'e3282d392ba4b4117d826bc968ce228a',
     beamseriesRes: '92aa987b1188bf082911e23cbaabb51b',
   },
   {
     label: 'Material Cutting Test - Simple',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
-    beamseriesRes: '92aa987b1188bf082911e23cbaabb51b',
+    adorRes: 'cb190e98c4a193db5773daebf6a66f4b',
+    beamseriesRes: '33377b55bedf39cbc59fa0fe95b8882b',
   },
   {
     label: 'Material Line Test',
@@ -66,11 +67,11 @@ const exampleList = [
   },
   {
     label: 'Material Printing Test',
-    adorRes: '18850d5792e9ae861b47a40ce5b412a7',
+    adorRes: '05e3c6a61f49072007e8b9990db6fd86',
   },
   {
     label: 'Acrylic Focus Probe - 3mm',
-    beamseriesRes: '92aa987b1188bf082911e23cbaabb51b',
+    beamseriesRes: '52d222a0a8ccb01c5919d6a4bd86948f',
   },
 ];
 
@@ -82,7 +83,7 @@ describe('example', () => {
   exampleList.forEach((example) => {
     it(`import ${example.label} with ador workarea`, () => {
       cy.changeWorkarea('Ador');
-      checkExample(example.label, example.adorRes, true);
+      checkExample(example.label, example.adorRes);
     });
 
     it(`import ${example.label} with beamseries workarea`, () => {
