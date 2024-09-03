f223d9afcb4ffba58a26e70a64794b3b13f74e3d
Dean Sung <dean@flux3dp.com>
2024-09-03 16:09:17 +0800

    Fix md5 for text-to-path

--- cypress/e2e/right-panel/text-to-path.spec.ts
+++ cypress/e2e/right-panel/text-to-path.spec.ts
@@ -126,8 +126,11 @@ describe.only('convert to path 2.0', () => {
     cy.get('#svg_2')
       .invoke('attr', 'd')
       .then((d) => {
-        if (isRunningAtGithub) expect(md5(d)).equal('5951b071eac21549437779d3bb3554bd');
-        else expect(md5(d)).equal('6fde8da297586452f7561d6dc93299bc');
+        let expectedValue = '6fde8da297586452f7561d6dc93299bc';
+        if (isRunningAtGithub) {
+          expectedValue = isWindows ? 'a93f7f803e8eea840d61531458838987' : '5951b071eac21549437779d3bb3554bd';
+        }
+        expect(md5(d)).equal(expectedValue);
       });
   });
 
@@ -152,8 +155,11 @@ describe.only('convert to path 2.0', () => {
     cy.get('#svg_2')
       .invoke('attr', 'd')
       .then((d) => {
-        if (isRunningAtGithub) expect(md5(d)).equal('9790fb3564249f0952dd1b131b77eba3');
-        else expect(md5(d)).equal('0b39368fe65b64e08cd08b9c8ff625b9');
+        let expectedValue = '0b39368fe65b64e08cd08b9c8ff625b9';
+        if (isRunningAtGithub) {
+          expectedValue = isWindows ? '415ea1b80d7fc380646248203bfd10e4' : '9790fb3564249f0952dd1b131b77eba3';
+        }
+        expect(md5(d)).equal(expectedValue);
       });
   });
 
@@ -169,8 +175,11 @@ describe.only('convert to path 2.0', () => {
     cy.get('#svg_2')
       .invoke('attr', 'd')
       .then((d) => {
-        if (isRunningAtGithub) expect(md5(d)).equal('8ffd98ed29e592cf37182bb6f9190729');
-        else expect(md5(d)).equal('d828dcc474ad48d26ecb9269cb3844fa');
+        let expectedValue = 'd828dcc474ad48d26ecb9269cb3844fa';
+        if (isRunningAtGithub) {
+          expectedValue = isWindows ? '9f6739e16128b247684722743e8b04a1' : '8ffd98ed29e592cf37182bb6f9190729';
+        }
+        expect(md5(d)).equal(expectedValue);
       });
   });
 });
