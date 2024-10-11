8c2db51ece1d36a0f1f5386bb494679e449f22b9
Jack Tsai <jack89040589@yahoo.com.tw>
2024-10-11 16:42:55 +0800

    Refactor code to remove redundant indentation and improve readability

--- cypress/e2e/canvas/different-dimension-with-machine.spec.ts
+++ cypress/e2e/canvas/different-dimension-with-machine.spec.ts
@@ -9,41 +9,28 @@ describe('machine selection and svg dimensions test', () => {
 
     // Iterate through each machine
     machines.forEach((machine) => {
-      // Click on the top bar menu container
       cy.get('.src-web-app-components-beambox-top-bar-TopBar-module__menu--Oh39C').click();
 
-      // Click on the "Edit" submenu
       cy.contains('li.rc-menu__submenu', 'Edit').click();
 
-      // Click on "Document Settings"
       cy.contains('li[role="menuitem"]', 'Document Settings').click();
-
-      // Select the machine from the dropdown
 
       cy.get('span.ant-select-selection-item').click({ multiple: true, force: true });
       cy.contains('.ant-select-item-option-content', machine).click();
 
-      // Click the confirm button
       cy.contains('span', 'Save')
         .should('be.visible')
         .and('not.be.disabled')
         .click({ force: true });
 
-      // Get and store the SVG dimensions
       cy.get('#svgroot').then(($svg) => {
         svgDimensions[machine] = {
           y: parseFloat($svg.attr('y'))
         };
       });
 
-      // Log the svgDimensions object using cy.log
       cy.wrap(svgDimensions).then((dims) => {
         const y = [];
-
-        // for (const dim in dims) {
-        //   cy.log(`dims[${dim}]: ${dims[dim].y}`);
-        //   y.push(dims[dim].y);
-        // }
 
         Object.keys(dims).forEach((dim) => {
           cy.log(`dims[${dim}]: ${dims[dim].y}`);
@@ -51,7 +38,6 @@ describe('machine selection and svg dimensions test', () => {
         });
 
         const set1 = new Set(y);
-        // set1 length = y length
         expect(set1.size).to.equal(y.length);
       });
     });
